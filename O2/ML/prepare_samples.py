5b919e0775204882acf325762e4a1dfdcaaf2d25
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2022-05-10 10:46:23 +0200

    Do not refilter input if file already there unless forced by user

--- O2/ML/prepare_samples.py
+++ O2/ML/prepare_samples.py
@@ -53,7 +53,7 @@ def divide_df_for_origin(df, cols_to_remove=['fFlagOrigin'], channel=None):
     return df_prompt, df_nonprompt, df_bkg
 
 
-def main(input_dir, max_files=1000, downscale_bkg=1.): #pylint: disable=too-many-locals,too-many-branches
+def main(input_dir, max_files=1000, downscale_bkg=1., force=False): #pylint: disable=too-many-locals,too-many-branches
     """
     Main function
 
@@ -74,57 +74,76 @@ def main(input_dir, max_files=1000, downscale_bkg=1.): #pylint: disable=too-many
     df_2p, df_3p = None, None
     with alive_bar(len(input_files[:max_files])) as bar_alive:
         for file in input_files[:max_files]:
-            file_root = uproot.open(file)
-            list_of_2p_df, list_of_3p_df = [], []
-            for tree_name in file_root.keys():
-                if "O2hftrigtrain2p" in tree_name:
-                    list_of_2p_df.append(f"{file}:{tree_name}")
-                elif "O2hftrigtrain3p" in tree_name:
-                    list_of_3p_df.append(f"{file}:{tree_name}")
-            df_2p = uproot.concatenate(list_of_2p_df, library="pd")
-            df_3p = uproot.concatenate(list_of_3p_df, library="pd")
+            file_root = uproot.open(file)          
+            indir = os.path.split(file)[0]
 
             # 2-prongs --> only D0
-            df_2p_prompt, df_2p_nonprompt, df_2p_bkg = divide_df_for_origin(df_2p)
-            df_2p_bkg = df_2p_bkg.sample(frac=downscale_bkg, random_state=42)
-            df_2p_prompt.to_parquet(
-                os.path.join(os.path.split(file)[0], "Prompt_D0ToKPi.parquet.gzip"),
-                compression="gzip"
-            )
-            df_2p_nonprompt.to_parquet(
-                os.path.join(os.path.split(file)[0], "Nonprompt_D0ToKPi.parquet.gzip"),
-                compression="gzip"
-            )
-            df_2p_bkg.to_parquet(
-                os.path.join(os.path.split(file)[0], "Bkg_D0ToKPi.parquet.gzip"),
-                compression="gzip"
-            )
-            df_2p = None
+            is_d0_filtered = False
+            for exfile in os.listdir(indir):
+                if "D0ToKPi.parquet.gzip" in exfile:
+                    is_d0_filtered = True
+                    break
+            if not is_d0_filtered or force:
+                list_of_2p_df = []
+                for tree_name in file_root.keys():
+                    if "O2hftrigtrain2p" in tree_name:
+                        list_of_2p_df.append(f"{file}:{tree_name}")
+                df_2p = uproot.concatenate(list_of_2p_df, library="pd")
+
+                df_2p_prompt, df_2p_nonprompt, df_2p_bkg = divide_df_for_origin(df_2p)
+                df_2p_bkg = df_2p_bkg.sample(frac=downscale_bkg, random_state=42)
+                df_2p_prompt.to_parquet(
+                    os.path.join(indir, "Prompt_D0ToKPi.parquet.gzip"),
+                    compression="gzip"
+                )
+                df_2p_nonprompt.to_parquet(
+                    os.path.join(indir, "Nonprompt_D0ToKPi.parquet.gzip"),
+                    compression="gzip"
+                )
+                df_2p_bkg.to_parquet(
+                    os.path.join(indir, "Bkg_D0ToKPi.parquet.gzip"),
+                    compression="gzip"
+                )
+                df_2p = None
 
             # 3-prongs --> D+, Ds+, Lc+, Xic+
+            is_3p_filtered = False
             for channel_3p in bits_3p:
-                flags = df_3p["fHFSelBit"].astype(int) & 2**bits_3p[channel_3p]
-                df_channel_3p = df_3p[flags.astype("bool").to_numpy()]
-                df_3p_prompt, df_3p_nonprompt, df_3p_bkg = divide_df_for_origin(
-                    df_channel_3p,
-                    ["fFlagOrigin", "fChannel", "fHFSelBit"],
-                    channel=channels_3p[channel_3p]
-                )
-                df_3p_bkg = df_3p_bkg.sample(frac=downscale_bkg, random_state=42)
-                df_3p_prompt.to_parquet(
-                    os.path.join(os.path.split(file)[0], f"Prompt_{channel_3p}.parquet.gzip"),
-                    compression="gzip"
-                )
-                df_3p_nonprompt.to_parquet(
-                    os.path.join(os.path.split(file)[0], f"Nonprompt_{channel_3p}.parquet.gzip"),
-                    compression="gzip"
-                )
-                df_3p_bkg.to_parquet(
-                    os.path.join(os.path.split(file)[0], f"Bkg_{channel_3p}.parquet.gzip"),
-                    compression="gzip"
-                )
+                for exfile in os.listdir(indir):
+                    if f"{channel_3p}.parquet.gzip" in exfile:
+                        is_3p_filtered = True
+                        break
 
-            df_3p = None
+            list_of_3p_df = []
+            if not is_3p_filtered or force:
+                for tree_name in file_root.keys():
+                    if "O2hftrigtrain3p" in tree_name:
+                        list_of_3p_df.append(f"{file}:{tree_name}")
+                df_3p = uproot.concatenate(list_of_3p_df, library="pd")
+
+                for channel_3p in bits_3p:
+                    flags = df_3p["fHFSelBit"].astype(int) & 2**bits_3p[channel_3p]
+                    df_channel_3p = df_3p[flags.astype("bool").to_numpy()]
+                    df_3p_prompt, df_3p_nonprompt, df_3p_bkg = divide_df_for_origin(
+                        df_channel_3p,
+                        ["fFlagOrigin", "fChannel", "fHFSelBit"],
+                        channel=channels_3p[channel_3p]
+                    )
+                    df_3p_bkg = df_3p_bkg.sample(frac=downscale_bkg, random_state=42)
+                    df_3p_prompt.to_parquet(
+                        os.path.join(indir, f"Prompt_{channel_3p}.parquet.gzip"),
+                        compression="gzip"
+                    )
+                    df_3p_nonprompt.to_parquet(
+                        os.path.join(indir, f"Nonprompt_{channel_3p}.parquet.gzip"),
+                        compression="gzip"
+                    )
+                    df_3p_bkg.to_parquet(
+                        os.path.join(indir, f"Bkg_{channel_3p}.parquet.gzip"),
+                        compression="gzip"
+                    )
+
+                df_3p = None
 
             bar_alive()
 
@@ -136,6 +155,8 @@ if __name__ == "__main__":
                         help="max input files to be processed")
     parser.add_argument("--downscale_bkg", type=float, default=1.,
                         help="fraction of bkg to be kept")
+    parser.add_argument("--force", action="store_true", default=False,
+                        help="force re-creation of output files")
     args = parser.parse_args()
 
-    main(args.input_dir, args.max_files, args.downscale_bkg)
+    main(args.input_dir, args.max_files, args.downscale_bkg, args.force)
